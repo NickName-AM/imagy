639255f3aec09182a86205a83ef252e099391209
NickName-AM <maharjanebik061@gmail.com>
2024-03-01 00:41:13 +0545

    rm art

--- art/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('', views.PostListView.as_view(), name='post-home'),
-    path('create/', views.PostCreateView.as_view(), name='post-create'),
-]
