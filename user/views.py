982e5f6802c763dde1aa9d1e6f56ce65fd99e15d
NickName-AM <maharjanebik061@gmail.com>
2024-02-29 23:16:24 +0545

    test1

--- user/views.py
+++ user/views.py
@@ -1,21 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from .forms import UserCreationForm, LoginForm
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
-
-def user_signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-        else:
-            form = UserCreationForm()
-        return render(request, 'signup.html', {'form':form})
-
-def user_logout(request):
-    logout(request)
-    return redirect('login')
