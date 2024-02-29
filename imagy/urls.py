d3e318a65941511647849732db5fc10e4065b7a8
b0tish <maharjan.saugat1@gmail.com>
2024-03-01 00:30:44 +0545

    views

--- imagy/urls.py
+++ imagy/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('post.urls'))
-    path('user/', user.urls),
+    path('', include('post.urls')),
+    path('user/', include('user.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
