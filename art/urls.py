24d7526979e63906041d7655e60795b350462488
NickName-AM <maharjanebik061@gmail.com>
2024-02-29 21:44:31 +0545

    first commit

---
+++ art/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.ArtListView.as_view(), name='art-home'),
+    path('create/', views.ArtCreateView.as_view(), name='art-create'),
+]
---
+++ art/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from art.models import Art
+
+# Create your views here.
+
+class ArtListView(ListView):
+    model = Art
+    template_name = 'art/index.html'
+    context_object_name = 'arts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        return context
+
+class ArtCreateView(CreateView):
+    model = Art
+    fields = ['title', 'image', 'tags']
+    template_name = 'art/create.html'
